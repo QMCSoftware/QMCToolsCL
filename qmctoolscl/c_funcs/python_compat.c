c1544281fc9b0f13e2965532e7fe55f311e3500c
alegresor <agsorokin3@gmail.com>
2026-09-07 09:29:52 -0500

    v1.2.4 more wheels updeas

--- qmctoolscl/c_funcs/python_compat.c
+++ qmctoolscl/c_funcs/python_compat.c
@@ -1,13 +1,10 @@
-// in Windows, you must define an initialization function for your extension
-// because setuptools will build a .pyd file, not a DLL
-// https://stackoverflow.com/questions/34689210/error-exporting-symbol-when-building-python-c-extension-in-windows
-
 #include "qmctoolscl.h"
 
+/* On Windows, setuptools builds a .pyd and the linker requires a
+   PyInit_<name> symbol to be exported.  On POSIX the shared object is
+   loaded with ctypes.CDLL, never imported as a module, so no CPython
+   symbols -- and therefore no Python.h / python3-dev -- are needed. */
+#ifdef _WIN32
 #include <Python.h>
-
-PyMODINIT_FUNC PyInit_c_lib(void)
-{
-    // do stuff...
-    printf("");
-}
+PyMODINIT_FUNC PyInit_c_lib(void) { return NULL; }
+#endif
